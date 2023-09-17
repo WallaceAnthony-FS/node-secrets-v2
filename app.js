1ffdde7be2b8d2fc66ce9023e40de84bab9652dc
Anthony Wallace <anthonyt.wallace@icloud.com>
2023-09-17 09:44:06 -0700

    v2

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+
+require("dotenv").config();
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+
+const dir = __dirname;
+const filenames = fs.readdirSync(dir);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  let files = filenames
+    .map((filename) => {
+      return `${filename}\n`;
+    })
+    .join("");
+
+  res.end(files);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
